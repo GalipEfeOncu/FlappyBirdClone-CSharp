f9fa9b99751430008203c1e20b81cb90b7bfecfa
Galip Efe Öncü <galipefe75@gmail.com>
2025-09-29 22:33:28 +0300

    Add files via upload

---
+++ FlappyBird/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace FlappyBird
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBird = new System.Windows.Forms.PictureBox();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.pictureGround = new System.Windows.Forms.PictureBox();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblStartText = new System.Windows.Forms.Label();
+            this.lblDeathText = new System.Windows.Forms.Label();
+            this.lblRestartText = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBird)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureGround)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBird
+            // 
+            this.pictureBird.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBird.Image = global::FlappyBird.Properties.Resources.yellowbird_midflap;
+            this.pictureBird.Location = new System.Drawing.Point(236, 198);
+            this.pictureBird.Name = "pictureBird";
+            this.pictureBird.Size = new System.Drawing.Size(68, 48);
+            this.pictureBird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBird.TabIndex = 0;
+            this.pictureBird.TabStop = false;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 15;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // pictureGround
+            // 
+            this.pictureGround.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pictureGround.Image = global::FlappyBird.Properties.Resources._base;
+            this.pictureGround.Location = new System.Drawing.Point(0, 556);
+            this.pictureGround.Name = "pictureGround";
+            this.pictureGround.Size = new System.Drawing.Size(1264, 125);
+            this.pictureGround.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureGround.TabIndex = 1;
+            this.pictureGround.TabStop = false;
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.BackColor = System.Drawing.Color.Transparent;
+            this.lblScore.Location = new System.Drawing.Point(12, 9);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(38, 13);
+            this.lblScore.TabIndex = 2;
+            this.lblScore.Text = "Score:";
+            // 
+            // lblStartText
+            // 
+            this.lblStartText.AutoSize = true;
+            this.lblStartText.Font = new System.Drawing.Font("Microsoft Sans Serif", 32.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStartText.Location = new System.Drawing.Point(421, 9);
+            this.lblStartText.Name = "lblStartText";
+            this.lblStartText.Size = new System.Drawing.Size(430, 51);
+            this.lblStartText.TabIndex = 3;
+            this.lblStartText.Text = "Press Space To Start";
+            // 
+            // lblDeathText
+            // 
+            this.lblDeathText.AutoSize = true;
+            this.lblDeathText.Font = new System.Drawing.Font("Microsoft Sans Serif", 32.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDeathText.Location = new System.Drawing.Point(467, 198);
+            this.lblDeathText.Name = "lblDeathText";
+            this.lblDeathText.Size = new System.Drawing.Size(279, 51);
+            this.lblDeathText.TabIndex = 4;
+            this.lblDeathText.Text = "You are dead";
+            // 
+            // lblRestartText
+            // 
+            this.lblRestartText.AutoSize = true;
+            this.lblRestartText.Font = new System.Drawing.Font("Microsoft Sans Serif", 32.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRestartText.Location = new System.Drawing.Point(421, 262);
+            this.lblRestartText.Name = "lblRestartText";
+            this.lblRestartText.Size = new System.Drawing.Size(386, 51);
+            this.lblRestartText.TabIndex = 5;
+            this.lblRestartText.Text = "Press R To Restart";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cyan;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.lblRestartText);
+            this.Controls.Add(this.lblDeathText);
+            this.Controls.Add(this.lblStartText);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.pictureGround);
+            this.Controls.Add(this.pictureBird);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Flappy Bird Clone";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBird)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureGround)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBird;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.PictureBox pictureGround;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblStartText;
+        private System.Windows.Forms.Label lblDeathText;
+        private System.Windows.Forms.Label lblRestartText;
+    }
+}
+
---
+++ FlappyBird/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlappyBird
+{
+    public partial class Form1 : Form
+    {
+        // Player physics
+        private int velocity = 0;
+        private const int gravity = 1;
+        private const int jumpPower = -11;
+        private const int maxVelocity = 1;
+
+        // Pipe settings
+        private const int pipeWidth = 75;
+        private int pipeSlidingSpeed = 3;
+        private int pipeSpawnInterval = 200;
+        private const int minGap = 150;
+        private const int maxGap = 250;
+        private float minTopPipeHeight;
+        private float maxTopPipeHeight;
+        private int pipeSpawnCounter = 350;
+        private readonly List<PictureBox> pipes = new List<PictureBox>();
+
+        // Form settings
+        private const int formWidth = 1280;
+        private const int formHeight = 720;
+
+        // Stats
+        private int score;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            gameTimer.Tick += gameTimer_Tick;
+            Form form = this;
+            form.Width = formWidth;
+            form.Height = formHeight;
+            form.MinimizeBox = false;
+            form.FormBorderStyle = FormBorderStyle.FixedSingle;
+            form.StartPosition = FormStartPosition.CenterScreen;
+
+            lblScore.Font = new Font(lblScore.Font.FontFamily, 24, lblScore.Font.Style);
+            lblScore.Left = 25;
+            lblScore.Top = 25;
+
+            minTopPipeHeight = (form.ClientSize.Height - pictureGround.Height) * 0.3f;
+            maxTopPipeHeight = (form.ClientSize.Height - pictureGround.Height) * 0.7f;
+
+            SetTextPositions();
+
+            lblDeathText.Visible = false;
+            lblRestartText.Visible = false;
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            velocity += gravity;
+            pictureBird.Top += velocity;
+
+            if (pictureBird.Top < 0) pictureBird.Top = 0;
+            if (pictureBird.Bottom > this.Height) pictureBird.Top = this.Height - pictureBird.Height;
+
+            if (velocity > maxVelocity) velocity = maxVelocity;
+            if (velocity < jumpPower) velocity = jumpPower;
+
+
+            if (score >= 20)
+            {
+                pipeSlidingSpeed = 4;
+                pipeSpawnInterval = 175;
+            }
+
+            else if (score >= 10)
+            {
+                pipeSlidingSpeed = 3;
+                pipeSpawnInterval = 233;
+            }
+
+            else
+            {
+                pipeSlidingSpeed = 2;
+                pipeSpawnInterval = 350;
+            }
+
+            //pipe kodları
+
+            //pipe Spawn
+            pipeSpawnCounter++;
+            if (pipeSpawnCounter >= pipeSpawnInterval)
+            {
+                CreatePipe();
+                pipeSpawnCounter = 0;
+            }
+
+            //pipe hareketi
+            foreach (var pipe in pipes)
+            {
+                pipe.Left -= pipeSlidingSpeed;
+
+                if (pipe.Tag?.ToString() == "pipe" && pipe.Right < pictureBird.Left)
+                {
+                    score++;
+                    lblScore.Text = "Score: " + score;
+                    pipe.Tag = "scored";
+                }
+
+                if (pipe.Right < 0)
+                {
+                    this.Controls.Remove(pipe);
+                    pipes.Remove(pipe);
+                    break;
+                }
+            }
+
+            //Collider kodları
+            foreach (var pipe in pipes)
+            {
+                if (pictureBird.Bounds.IntersectsWith(pipe.Bounds))
+                {
+                    gameTimer.Stop();
+                    lblDeathText.Visible = true;
+                    lblRestartText.Visible = true;
+                }
+            }
+
+            if (pictureBird.Top < 0 || pictureBird.Bottom > this.Height)
+            {
+                gameTimer.Stop();
+                lblDeathText.Visible = true;
+                lblRestartText.Visible = true;
+            }
+
+            if (pictureBird.Bounds.IntersectsWith(pictureGround.Bounds))
+            {
+                gameTimer.Stop();
+                lblDeathText.Visible = true;
+                lblRestartText.Visible = true;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                if (!lblDeathText.Visible)
+                {
+                    gameTimer.Start();
+                    lblStartText.Visible = false;
+                    velocity = jumpPower;
+                }
+            }
+
+            if (e.KeyCode == Keys.R)
+            {
+                if (lblDeathText.Visible)
+                {
+                    RestartGame();
+                }
+            }
+        }
+
+        private void CreatePipe()
+        {
+            Random rnd = new Random();
+            int gap = rnd.Next(minGap, maxGap);
+            float pipeHeight = rnd.Next((int)minTopPipeHeight, (int)maxTopPipeHeight);
+
+            PictureBox topPipe = new PictureBox();
+            topPipe.Image = Properties.Resources.pipe_green1;
+            topPipe.SizeMode = PictureBoxSizeMode.StretchImage;
+            topPipe.Width = pipeWidth;
+            topPipe.Height = (int)pipeHeight;
+            topPipe.Top = 0;
+            topPipe.Left = this.Width;
+            this.Controls.Add(topPipe);
+            pipes.Add(topPipe);
+
+            PictureBox bottomPipe = new PictureBox();
+            bottomPipe.Image = Properties.Resources.pipe_green;
+            bottomPipe.SizeMode = PictureBoxSizeMode.StretchImage;
+            bottomPipe.Width = pipeWidth;
+            bottomPipe.Height = this.Height - (int)pipeHeight - gap;
+            bottomPipe.Top = (int)pipeHeight + gap;
+            bottomPipe.Left = this.Width;
+            bottomPipe.Tag = "pipe";
+            this.Controls.Add(bottomPipe);
+            pipes.Add(bottomPipe);
+        }
+
+        private void RestartGame()
+        {
+            gameTimer.Stop();
+
+            pictureBird.Top = this.ClientSize.Height / 2 - pictureBird.Height / 2;
+            velocity = 0;
+
+            foreach (var pipe in pipes)
+            {
+                this.Controls.Remove(pipe);
+            }
+            pipes.Clear();
+
+            score = 0;
+            lblScore.Text = "Score: 0";
+
+            lblDeathText.Visible = false;
+            lblRestartText.Visible = false;
+            lblStartText.Visible = true;
+
+            pipeSlidingSpeed = 3;
+            pipeSpawnInterval = 200;
+        }
+
+        private void SetTextPositions()
+        {
+            lblStartText.Left = ((this.ClientSize.Width - lblStartText.Width) / 2);
+            lblStartText.Top = ((this.ClientSize.Height - lblStartText.Height - pictureGround.Height) / 2);
+            lblStartText.Font = new Font(lblStartText.Font.FontFamily, 32, lblStartText.Font.Style);
+
+            lblDeathText.Left = ((this.ClientSize.Width - lblDeathText.Width) / 2);
+            lblDeathText.Top = ((this.ClientSize.Height - lblDeathText.Height - pictureGround.Height) / 2) - lblRestartText.Height;
+            lblDeathText.Font = new Font(lblDeathText.Font.FontFamily, 32, lblDeathText.Font.Style);
+
+            lblRestartText.Left = ((this.ClientSize.Width - lblRestartText.Width) / 2);
+            lblRestartText.Top = ((this.ClientSize.Height - lblRestartText.Height - pictureGround.Height) / 2);
+            lblRestartText.Font = new Font(lblRestartText.Font.FontFamily, 32, lblRestartText.Font.Style);
+        }
+    }
+}
